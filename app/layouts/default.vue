5461aa84b21b452411298214af1e4945237b253f
Csituka_D <694428809@qq.com>
2026-09-12 16:35:14 +0800

    Update default.vue

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,10 +1,10 @@
 <template>
   <v-app>
-    <v-app-bar color="primary" flat theme="dark">
-      <div class="d-flex align-center w-100 px-6">
-        <v-app-bar-title>MGL</v-app-bar-title>
+    <v-app-bar color="purple-lighten-4" flat>
+      <div class="d-flex align-center w-100 px-10">
+        <img src="/icon-full.png" alt="Magical Land" class="app-logo">
         <v-spacer />
-        <v-tabs color="white" class="flex-grow-0">
+        <v-tabs color="purple-darken-3" class="flex-grow-0">
           <v-tab to="/">主页</v-tab>
           <v-tab to="/docs">文档</v-tab>
         </v-tabs>
@@ -16,3 +16,11 @@
     </v-main>
   </v-app>
 </template>
+
+<style scoped>
+.app-logo {
+  display: block;
+  height: 36px;
+  width: auto;
+}
+</style>
